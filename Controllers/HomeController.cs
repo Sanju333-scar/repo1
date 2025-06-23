bb04b36175f6f2554528c39aac80bc0dafb482fa
Sanju333-scar <bheeman1000@gmail.com>
2025-06-24 01:08:53 +0530

    all changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,7 +11,7 @@ public class HomeController : Controller
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
-    }
+    
 
     public IActionResult Index()
     {
